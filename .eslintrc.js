913fded7b36930e98a97944c0828f104afd3238d
Ender-Wiggin <117010097@link.cuhk.edu.cn>
2023-03-28 21:50:02 +0100

    uodate env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,44 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
   env: {
     browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
+    es2021: true,
+    node: true,
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
   rules: {
-    'react/prop-types': 'off',
-    'react/jsx-curly-brace-presence': 'error',
-    // React 17
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/self-closing-comp': [
-      'error',
-      {
-        component: true,
-        html: true,
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
       },
-    ],
-    'react/jsx-boolean-value': 'error',
-    'prefer-template': "error",
-    'jsx-quotes': ["error", "prefer-double"],
-    "react/jsx-tag-spacing": "error"
-  }
-}
+    },
+  ],
+};
