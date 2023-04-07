5699b56ea675e33beed861c7c160a7ca34897b1a
Paradixe <ParadixeChen@outlook.com>
2023-04-07 19:05:20 +0100

    Read & display non-verified providers' info

--- src/components/admin/VerifyPage.tsx
+++ src/components/admin/VerifyPage.tsx
@@ -38,20 +38,20 @@ const VerifyPage: React.FC = () => {
     fetchServices();
   }, []);
 
+  const non_verified_providers = providers.filter((provider) => provider.isVerified === false);
+
   return (
     <div className="py-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="bg-gray-100 w-full items-center justify-between">
           <h2 className="text-2xl font-medium text-gray-900">Need to Verify</h2>
           <div className="text-gray-500 text-sm">
-            <span className="font-medium">{providers.length}</span> items
+            <span className="font-medium">{non_verified_providers.length}</span> items
           </div>
         </div>
-        {providers.map((provider, index) => {
-          if (!provider.isVerified) {
-            return <ProviderItem key={index} provider={provider} />;
-          }
-        })}
+        {non_verified_providers.map((provider, index) => (
+          <ProviderItem key={index} provider={provider} />
+        ))}
       </div>
     </div>
   );
