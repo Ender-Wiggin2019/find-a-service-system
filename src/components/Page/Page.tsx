6812feefdaf3589a490c82226bbce0ee546145b6
Paradixe <ParadixeChen@outlook.com>
2023-05-08 22:58:15 +0100

    ui-detail page optimise

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -6,7 +6,11 @@ type PageProps = {
 }
 
 const Page: React.FC<PageProps> = ({ children }) => {
-    return <div className='w-full py-12 sm:w-3/4 md:w-w-4/5 lg:w-w-3/4 xl:w-3/4 2xl:w-3/4 mx-auto'>{children}</div>
+    return (
+        <div className='w-full py-12 sm:w-3/4 md:w-w-4/5 lg:w-w-3/4 xl:w-3/4 2xl:w-3/4 mx-auto flex flex-col'>
+            {children}
+        </div>
+    )
 }
 
 export default Page
